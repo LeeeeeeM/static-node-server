8e5ec5bd6d808d4e0a0b3c97d65be91f8fa3fb84
evilemon <slm_1990@126.com>
2018-02-04 20:05:19 +0800

    本地访问

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const serve = require('koa-static')
 const koa = require('koa')
 const helmet = require('koa-helmet')
+const cors = require('koa2-cors')
 const app = new koa()
 
 const router = require('./routes').produceRouter()
+
+app.use(cors({
+	origin: ctx => '*'
+}))
 
 app.use(helmet())
 
@@ -11,6 +16,7 @@ app.use(serve(__dirname + '/public'))
 
 app.use(router.routes(), router.allowedMethods())
 
-app.listen(8090, () => {
-        console.log('server begin on 8090')
+app.listen(8090, '127.0.0.1', () => {
+	// 这里是个坑,node 服务如果想用127.0.0.1访问必选加本机 127.0.0.1
+    console.log('server begin on 8090')
 })
